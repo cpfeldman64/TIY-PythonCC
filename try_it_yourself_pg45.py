334aaed5bbc011c1f595e076bb802db3d6b7cb74
Connor Feldman <80939702+cpfeldman64@users.noreply.github.com>
2023-09-04 19:25:05 -0500

    Add files via upload

---
+++ try_it_yourself_pg45.py
@@ -0,0 +1,29 @@
+# Try it yourself page 45
+
+places = [
+    'germany', 'france', 'japan', 'italy', 'greece'
+]
+
+print(places)
+
+sorted_places = sorted(places)
+print(f'\n{sorted_places}')
+
+print(f'\n{places}')
+
+print(f'\n{sorted(places, reverse=True)}')
+
+print(f'\n{places}')
+
+places.reverse()
+print(f'\n{places}')
+
+places.reverse()
+print(f'\n{places}')
+
+places.sort()
+print(f'\n{places}')
+
+places.sort(reverse=True)
+print(f'\n{places}')
+
