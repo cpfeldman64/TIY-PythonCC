6d25f07d64500f485b53aabb216769d19b849487
Connor Feldman <80939702+cpfeldman64@users.noreply.github.com>
2023-09-09 19:20:49 -0500

    Add files via upload

--- tiy_pg88.py
+++ tiy_pg88.py
@@ -22,14 +22,40 @@ else:
 
 # Checking Usernames
 current_users = ['James', 'Lily', 'Harry', 'Hermione', 'Ron']
-cased_users = [x.lower() for x in current_users]
-
-new_users = ['john', 'jane', 'lucy', 'lily', 'james']
+current_users_lower = ['james', 'lily', 'harry', 'hermione', 'ron']
+new_users = ['Joseph', 'Lauren', 'james', 'Lily']
 
 for new_user in new_users:
-    if new_user in current_users or cased_users:
+    if new_user  in current_users:
+        print("\nThis username is taken, please select another.")
+    elif new_user in current_users_lower:
         print("\nThis username is taken, please select another.")
     else:
         print("\nThat username is available!")
 
-# still not working, come back to it later!
+print('\n\n\n')
+
+# I've made it case insensitive, but I want to know how I can convert the whole
+# list to lowercase and store that as a variable.
+
+# In theory this could still work so long as when a new user is added they
+# are simulatenously populated to the current_users and current_users_lower
+# list. Still weird though. I feel like it could be cleaner but idk how.
+
+
+# Ordinal Numbers
+
+numbers = list(range(1,10))
+for number in numbers:
+    if number == 1:
+        print(f'{number}st!')
+    elif number == 2:
+        print(f'{number}nd!')
+    elif number == 3:
+        print(f'{number}rd!')
+    else:
+        print(f'{number}th!')
+
+# whoo!
+
+
